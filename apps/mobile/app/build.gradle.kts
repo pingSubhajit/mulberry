54cdec971310cb494c6b93653c8168f99ae6c94c
Subhajit Kundu <subha60kundu@gmail.com>
2026-04-24 01:03:25 +0530

    chore: turn on minification & reduce wallpaper bundle size

--- apps/mobile/app/build.gradle.kts
+++ apps/mobile/app/build.gradle.kts
@@ -107,7 +107,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
